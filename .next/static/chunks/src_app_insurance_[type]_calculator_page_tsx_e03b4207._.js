6e64f55742d88dedf3c85228b61c534599f90280
dioncx <148190661+dioncx@users.noreply.github.com>
2025-07-28 19:20:47 +0700

    Add .next/static/chunks/src_app_insurance_[type]_calculator_page_tsx_e03b4207._.js 1753705244176

---
+++ .next/static/chunks/src_app_insurance_[type]_calculator_page_tsx_e03b4207._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_318f6840._.js",
+  "static/chunks/node_modules_7a3399e5._.js"
+],
+    source: "dynamic"
+});
